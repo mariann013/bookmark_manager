d88024e4bb56b1d124102ab1e40f55ccd35f9b8d
Mari-Ann Meling <mariann013@gmail.com>
2015-11-04 10:57:38 +0000

    feature tests for links

---
+++ spec/features/viewing_links_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper.rb"
+
+feature "to view links" do
+
+  scenario "view links on homepage" do
+    Link.create(:url => "http://www.google.com", :title => "Google")
+    visit "/links"
+    within "ul#links" do
+      expect(page).to have_content "Google"
+    end
+  end
+
+end
